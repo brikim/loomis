5bb49d4189a83b5743b417949ca4cbca28d11095
Brian <bkimmle@gmail.com>
2026-01-06 20:49:38 -0600

    cmake improvements, added a dependencies.cmake

---
+++ Dependencies.cmake
@@ -0,0 +1,13 @@
+# Version Definitions
+set(GLAZE_VERSION   v6.5.1)
+set(SPDLOG_VERSION  v1.17.0)
+set(HTTPLIB_VERSION v0.30.0)
+set(PUGIXML_VERSION v1.15)
+set(CRONCPP_VERSION v2023.03.30)
+
+# Repository URLs (optional, but good for one-spot editing)
+set(GLAZE_REPO   https://github.com/stephenberry/glaze.git)
+set(SPDLOG_REPO  https://github.com/gabime/spdlog.git)
+set(HTTPLIB_REPO https://github.com/yhirose/cpp-httplib.git)
+set(PUGIXML_REPO https://github.com/zeux/pugixml.git)
+set(CRONCPP_REPO https://github.com/mariusbancila/croncpp.git)
