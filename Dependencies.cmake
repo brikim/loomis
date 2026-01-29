b84ec892aa6ec1cd76f453e0f11094f8c42fe3cb
Brian <bkimmle@gmail.com>
2026-01-28 18:33:50 -0600

    updated warp version to remove futures

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,5 +1,5 @@
 # Version Definitions
-set(WARP_VERSION  v2.0.10)
+set(WARP_VERSION  v2.0.13)
 set(GLAZE_VERSION v7.0.1)
 
 # Repository URLs (optional, but good for one-spot editing)
--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
 
 namespace loomis
 {
-   static constexpr const std::string_view LOOMIS_VERSION("v1.0.4");
+   static constexpr const std::string_view LOOMIS_VERSION("v1.0.5");
 }
